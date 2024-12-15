4c42b75973042893abeaef861431bd4214ff7549
maheshreddyy345 <maheshreddyy345@github.com>
2024-12-15 16:58:11 -0600

    Updated inject-env.js to use ES module syntax

--- inject-env.js
+++ inject-env.js
@@ -1,12 +1,17 @@
 // inject-env.js
-const fs = require('fs');
-const path = require('path');
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Read the template file
-const template = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+const template = readFileSync(join(__dirname, 'index.html'), 'utf8');
 
 // Replace the environment variable placeholder
 const html = template.replace('process.env.OPENAI_API_KEY', `"${process.env.OPENAI_API_KEY}"`);
 
 // Write the processed file
-fs.writeFileSync(path.join(__dirname, 'index.html'), html);
+writeFileSync(join(__dirname, 'index.html'), html);
