a36a275c0e22dfec18bf17ec8cf3058b2ed71b9d
maheshreddyy345 <maheshreddyy345@github.com>
2024-12-15 16:48:59 -0600

    Updated environment variable handling

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Use environment variable for API key
 const CONFIG = {
-    OPENAI_API_KEY: window.OPENAI_API_KEY || ''  // Will be set from Netlify environment
+    OPENAI_API_KEY: window.ENV?.OPENAI_API_KEY || ''
 };
 
 export default CONFIG;
